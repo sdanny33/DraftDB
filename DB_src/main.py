0f6084e7b0cfcf6e8c05cbda9d36231ded7a7072
sdanny33 <sdanny@vt.edu>
2026-04-09 00:41:17 -0400

    updated everything to 4/9

--- DB_src/main.py
+++ DB_src/main.py
@@ -1,9 +1,11 @@
 from createDB import main as create_db_main
 from replayScraper import main as replay_scraper_main
+from table import main as table_main
 
 def main():
     replay_scraper_main()
     create_db_main()
+    table_main()
 
 if __name__ == "__main__":
     main()
